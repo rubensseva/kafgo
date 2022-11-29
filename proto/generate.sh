cb48c5b29d7ff48170f1081a6bb7a6679ab26a17
rubensseva <r.sevaldson@gmail.com>
2022-11-29 17:10:06 +0100

    Fix proto gen script

--- proto/generate.sh
+++ proto/generate.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
 protoc --go_out=. --go_opt=paths=source_relative \
-    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-    --plugin=/home/ruben/go/bin kafgo.proto
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative kafgo.proto
